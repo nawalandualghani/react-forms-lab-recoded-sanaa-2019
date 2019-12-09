5f2389a9810a9f133675b2849c8f3e09f5dde82e
nawal abdualghani <nawalabdualghani@gmail.com>
2019-12-09 07:08:56 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,8 +4,24 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+     password: ''
+    };
   }
+  handleChange = event => {
+   this.setState({
+     [event.target.name]: event.target.value
+   })
+ }
+
+ handleSubmit = event => {
+   event.preventDefault()
+
+   if (!this.state.username || !this.state.password) return
+
+   this.props.handleLogin(this.state)
+ }
 
   render() {
     return (
